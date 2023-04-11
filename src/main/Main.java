77b7a295e8c59c8f05ce090f12cd055a44b82d56
GustavoVFA <gustavovfamorim@gmail.com>
2023-04-11 19:33:37 -0300

    Primeiro

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+
+package main;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Model.Mensagem_DAO.mostra();
+        Model.Mensagem_DAO.mostra2();
+        JOptionPane.showMessageDialog(null, Model.Mensagem_DAO.teste);
+    }
+    
+}
